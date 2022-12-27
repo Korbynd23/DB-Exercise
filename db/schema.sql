49890a6891c431953d8db148768a0508dbf5bb53
kdicari24@gmail.com <kdicari24@gmail.com>
2022-12-27 12:18:57 -0500

    resolved merge conflicts, added connection and server.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,31 @@
 DROP DATABASE IF EXISTS boty_db;
--- Creates the "Board of the Year" Database --
+
 CREATE DATABASE boty_db;
--- Makes it so all of the following code will affect boty_db --
+
 USE boty_db;
--- Creates the table "boty" within boty_db --
+
 CREATE TABLE boty (
-    id INT NOT NULL,
-    name VARCHAR(20) NOT NULL
+  id INT NOT NULL,
+  name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE meme (
-    -- Creates a numeric column called "id' which will automatically increment its default value as we create new rows" --
-    id INT NOT NULL,
-    name VARCHAR(20) NOT NULL
+  id INT NOT NULL,
+  name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE macropad (
-    id INT NOT NULL,
-    -- Makes a string column called "name" which CANNOT contain null -- 
-    name VARCHAR(20) NOT NULL
+  id INT NOT NULL,
+  name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE creative (
-    id INT NOT NULL,
-    name VARCHAR(20) NOT NULL 
+  id INT NOT NULL,
+  name VARCHAR(20) NOT NULL
 );
 
 INSERT INTO boty (id, name)
-VALUE
-    (1, "40rmie"),
-    (2, "Embrace"),
-    (3, "Brane")
+VALUES 
+    ( 1, "40rmie"),
+    ( 2, "Embrace"),
+    ( 3, "Brane");
